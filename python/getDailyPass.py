73de7ec29d5b4f4ab9d58615325bacfdd9754da0
Chris Herrera <christian@christian-herrera.com>
2021-10-10 18:53:49 -0700

    Updating imports

--- python/getDailyPass.py
+++ python/getDailyPass.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import requests
 import yaml
 
+try:
+    ff_opts = webdriver.FirefoxOptions()
+    ff_opts.headless = True
+    browser = webdriver.Firefox(options=ff_opts)
+except Exception as err:
+    print("Unable to initialize headless FireFox sessions with Selenium.")
+    print(err)
